5a4fb6c515c1d1b90aa5984ab4baa6f9218eded8
luongdang0701 <luongdang0701@gmail.com>
2021-02-12 16:06:41 -0800

    lastplz

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('vishakha', views.vishakha, name='blog-vishakha'),
-    path('rebecca', views.rebecca, name='blog-rebecca'),
-    path('luong', views.luong, name='blog-luong'),
-    path('bryan', views.bryan, name='blog-bryan'),
-    path('ashwini', views.ashwini, name='blog-ashwini'),
-    path('malcolm', views.malcolm, name='blog-malcolm'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
 ]
